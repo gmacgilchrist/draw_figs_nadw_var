74266a31da430caea2a84f216160cc9a44466cb2
Graeme MacGilchrist <graeme.macgilchrist@univ.ox.ac.uk>
2020-09-15 16:46:49 +0100

    added new notebooks

--- ariane_general.py
+++ ariane_general.py
@@ -1,5 +1,13 @@
 import xarray as xr
 def ariane_year(final_t,time,dim='nfile',yrst=0,secondsperyear=31536000):
+    '''Calculate the year of ventilation based on the time-index of ventilation
+    INPUT:  final_t ; array of time-indices
+            time    ; array of times (in seconds) for each index
+            dim     ; dimension in [time]
+            yrst    ; year of first model outut
+            secondsperyear
+    OUTPUT: final_year ; array (same length as final_t) of years'''
+    
     final_year = yrst + time.interp({dim:final_t})/secondsperyear
     final_year.name = 'final_year'
     return final_year
